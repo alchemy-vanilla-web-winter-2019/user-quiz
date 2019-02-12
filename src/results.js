45897a00f158824e99d1ff0e9486ed1b9cced1d5
Ben Parisot <me@benparisot.com>
2019-02-12 15:07:46 -0800

    add explanation message to results page

--- src/results.js
+++ src/results.js
@@ -9,6 +9,7 @@ else {
 }
 
 const resultsNode = document.getElementById('result');
+const explanationNode = document.getElementById('explanation');
 const picResultsNode = document.getElementById('pic-result');
 
 const finalResult = displayResult(answers);
@@ -18,26 +19,31 @@ resultsNode.textContent = finalResult;
 const pic = document.createElement('img');
 
 if(finalResult === 'banana') {
+    explanationNode.textContent = 'You are a strong, goofy person with a bit of a tropical flair about you.';
     pic.src = '/assets/banana.jpg';
     picResultsNode.appendChild(pic);
 }
 
 else if(finalResult === 'apple') {
+    explanationNode.textContent = 'You are a sweet, traditional person with a bit of a bite.';
     pic.src = '/assets/apple.jpg';
     picResultsNode.appendChild(pic);
 }
 
 else if(finalResult === 'blueberry') {
+    explanationNode.textContent = 'You are a dark, mysterious person with sophisticated tastes.';
     pic.src = '/assets/blueberry.png';
     picResultsNode.appendChild(pic);
 }
 
 else if(finalResult === 'kiwi') {
+    explanationNode.textContent = 'You are a wild-child with few worries and even fewer inhibitions.';
     pic.src = '/assets/kiwi.jpg';
     picResultsNode.appendChild(pic);
 }
 
 else {
+    explanationNode.textContent = 'You are inscrutable. Try taking the quiz again!';
     pic.src = '/assets/shrug.jpeg';
     picResultsNode.appendChild(pic);
 }
