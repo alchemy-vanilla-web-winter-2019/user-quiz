930644991201f33a883f0dee6214416b2dd60524
Sean Nelson <smnelsonn@gmail.com>
2019-02-12 11:51:18 -0800

    completed first question and test

--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,3 @@
-const test = QUnit.test;
+import './questions-tests/score-flower.tests.js';
 
-function scoreFlower(flower, scorecard) {
-    scorecard.red += 1;
-}
 
-test('score flowers: rose', function(assert) {
-    //arrange
-
-    const scorecard = { red: 0, blue: 0, yellow: 0 };
-
-    //act
-
-    scoreFlower('rose', scorecard);
-    //assert
-    assert.deepEqual(scorecard, { red: 1, blue: 0, yellow: 0 });
-});
