e1445d79d1ac93e5af3be88c8c1035fca9fe38e1
Sean Nelson <smnelsonn@gmail.com>
2019-02-11 16:03:39 -0800

    first test round is done

--- test/favorite/score-favorite.test.js
+++ test/favorite/score-favorite.test.js
@@ -14,3 +14,12 @@ test('Ocean is selected as favorite', function(assert){
     assert.deepEqual(scorecard, { ocean: 1, sun: 0, grass: 0 });
 });
 
+test('Sun is selected as favorite', function(assert){
+    scoreFavorite('sun', scorecard);
+    assert.deepEqual(scorecard, { ocean: 0, sun: 1, grass: 0 });
+});
+test('Grass is selected as favorite', function(assert){
+    scoreFavorite('grass', scorecard);
+    assert.deepEqual(scorecard, { ocean: 0, sun: 0, grass: 1 });
+});
+
