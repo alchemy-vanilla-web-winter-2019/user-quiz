67762c88d250953e389e2d917eca9e051b22bf65
Cheri Portman <cheriportman@hotmail.com>
2019-02-12 15:28:40 -0800

    first of the tests passes

--- src/time-score-crunch/time-score-crunch.js
+++ src/time-score-crunch/time-score-crunch.js
@@ -1,5 +1,13 @@
-function timeScoreCrunch() {
-    console.log('hi');
+function timeScoreCrunch(choice, scorecard) {
+    if(choice === 'morning') {
+        scorecard.red += 0.5;
+        scorecard.purple += 0.5;
+        scorecard.yellow += 1;
+        scorecard.green += 0.5;
+    }
+
+
+
 }
 
 
