f314534ed54ddb635a714b1fbe444bef6cacf911
Ben Parisot <me@benparisot.com>
2019-02-12 14:52:57 -0800

    update display results function and index.js

--- src/eval/display-results.js
+++ src/eval/display-results.js
@@ -2,13 +2,25 @@ import checkColor from '../eval/calculate/check-color.js';
 import checkAnimal from '../eval/calculate/check-animal.js';
 import checkMovie from '../eval/calculate/check-movie.js';
 import rateScorecard from '../eval/calculate/rate-scorecard.js';
+import checkCurse from './calculate/check-curse.js';
+import checkDessert from './calculate/check-dessert.js';
+import checkLeisure from './calculate/check-leisure.js';
+import checkRelationship from './calculate/check-relationship.js';
+import checkTransportation from './calculate/check-transportation.js';
+import checkVacation from './calculate/check-vacation.js';
 
 function displayResults(answers) {
     let scorecard = { apple: 0, banana: 0, blueberry: 0, kiwi: 0 };
 
-    checkColor(answers.color, scorecard);
     checkAnimal(answers.animal, scorecard);
+    checkColor(answers.color, scorecard);
+    checkCurse(answers.curse, scorecard);
+    checkDessert(answers.dessert, scorecard);
+    checkLeisure(answers.leisure, scorecard);
     checkMovie(answers.movie, scorecard);
+    checkRelationship(answers.relationship, scorecard);
+    checkTransportation(answers.transportation, scorecard);
+    checkVacation(answers.vacation, scorecard);
     const result = rateScorecard(scorecard);
     return result;
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,25 @@ fruitQuizNode.addEventListener('submit', function(event) {
     const color = formData.get('color');
     const animal = formData.get('animal');
     const movie = formData.get('movie');
+    const leisure = formData.get('activity');
+    const vacation = formData.get('vacation');
+    const transportation = formData.get('transportation');
+    const curse = formData.get('curse');
+    const dessert = formData.get('dessert');
+    const relationship = formData.get('relationship');
+    const lobster = formData.get('lobster');
 
     const answers = {
         color: color,
         animal: animal,
-        movie: movie
+        movie: movie,
+        leisure: leisure,
+        vacation: vacation,
+        transportation: transportation,
+        curse: curse,
+        dessert: dessert,
+        relationship: relationship,
+        lobster: lobster
     };
     
     const answersJSON = JSON.stringify(answers);
