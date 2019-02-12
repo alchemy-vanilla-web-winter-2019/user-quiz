00e6daf84198b9544955faba2876d85faa01b261
Ben Parisot <me@benparisot.com>
2019-02-11 22:28:25 -0800

    pass tests for other answer types

--- test/display-results.test.js
+++ test/display-results.test.js
@@ -23,3 +23,27 @@ test('if apple has the highest score return apple', function(assert) {
 
     assert.equal(result, expected);
 });
+
+test('if banana has the highest score return banana', function(assert) {
+    const answers = { color: 'yellow', animal: 'monkey', movie: 'saw' };
+    const expected = 'banana';
+    const result = displayResults(answers);
+
+    assert.equal(result, expected);
+});
+
+test('if kiwi has the highest score return kiwi', function(assert) {
+    const answers = { color: 'green', animal: 'pig', movie: 'saw' };
+    const expected = 'kiwi';
+    const result = displayResults(answers);
+
+    assert.equal(result, expected);
+});
+
+test('if blueberry has the highest score return blueberry', function(assert) {
+    const answers = { color: 'blue', animal: 'crow', movie: 'saw' };
+    const expected = 'blueberry';
+    const result = displayResults(answers);
+
+    assert.equal(result, expected);
+});
