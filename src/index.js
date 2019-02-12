1e1939b705e51e566678ad4675ca2e243f24d2e2
Ben Parisot <me@benparisot.com>
2019-02-11 21:44:54 -0800

    get values from form, store in object and set in localstorage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+
+const fruitQuizNode = document.getElementById('fruit-quiz');
+
+
+fruitQuizNode.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(fruitQuizNode);
+    const color = formData.get('color');
+    const animal = formData.get('animal');
+    const movie = formData.get('movie');
+
+    const answers = {
+        color: color,
+        animal: animal,
+        movie: movie
+    };
+    console.log(answers);
+
+    const answersJSON = JSON.stringify(answers);
+    window.localStorage.setItem('answers', answersJSON);
+});
