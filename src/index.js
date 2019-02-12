725f29be0a590b35baa0a20826b79151c77e968d
Cara Ponzini <ponzini@gmail.com>
2019-02-11 20:48:42 -0800

    added welcome page and name input

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const nameForm = document.getElementById('name-form');
+//const userName = document.getElementById('name');
+
+nameForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('hi');
+
+});
