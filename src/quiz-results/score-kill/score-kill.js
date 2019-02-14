6960bfd48450be5f5430e2fac00876a044c351c7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-13 19:19:33 -0800

    still needs more testing

--- src/quiz-results/score-kill/score-kill.js
+++ src/quiz-results/score-kill/score-kill.js
@@ -1,14 +1,26 @@
 function scoreKill(kill, scorecard) {    
     if(kill.length === 1) {
-        if(kill[0] === 'honor') {
-            scorecard.mercer += 1;
-            scorecard.colville += 0.5;
-            scorecard.perkins += 0.5;
-        }
-        if(kill[0] === 'dishonor') {
-            scorecard.mercer -= 1;        
-            scorecard.perkins += 1;
-        }
+        switch(kill[0]) {
+            case 'honor':
+                scorecard.mercer += 1;
+                scorecard.colville += 0.5;
+                scorecard.perkins += 0.5;
+                break;
+            case 'dishonor':
+                scorecard.mercer -= 1;        
+                scorecard.perkins += 1;
+                break;
+            case 'drama':
+                scorecard.colville += 1;
+                scorecard.mercer += 1;
+                break;
+            case 'poor':
+                scorecard.colville += 1;
+                scorecard.perkins += 1;
+                break;
+            default:
+                break;
+        }        
     }
     if(kill.length === 0) {
         scorecard.mercer += 1;
--- test/quiz-results/score-kill/score-kill.test.js
+++ test/quiz-results/score-kill/score-kill.test.js
@@ -10,20 +10,32 @@ QUnit.testStart(function() {
     scorecard = { colville: 0, mercer: 0, perkins: 0 };
 });
 
-test('if no checkboxes are checked incriment mercer by 1 and decrement others by 1', function(assert) {
+test('if no checkboxes are checked increment mercer by 1 and decrement others by 1', function(assert) {
     const kill = [];
     scoreKill(kill, scorecard);
     assert.deepEqual(scorecard, { colville: -1, mercer: 1, perkins: -1 });
 });
 
-test('if just honor is checked incriment mercer by 1 and others by 0.5', function(assert) {
+test('if just honor is checked increment mercer by 1 and others by 0.5', function(assert) {
     const kill = ['honor'];
     scoreKill(kill, scorecard);
     assert.deepEqual(scorecard, { colville: 0.5, mercer: 1, perkins: 0.5 });
 });
 
-test('if just dishonor is checked incriment perkins by 1 and decrement mercer by 1', function(assert) {
+test('if just dishonor is checked increment perkins by 1 and decrement mercer by 1', function(assert) {
     const kill = ['dishonor'];
     scoreKill(kill, scorecard);
     assert.deepEqual(scorecard, { colville: 0, mercer: -1, perkins: 1 });
+});
+
+test('if only drama is chosen increment colville and mercer by 1', function(assert) {
+    const kill = ['drama'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 1, mercer: 1, perkins: 0 });
+});
+
+test('if only poor is chosen increment colville and perkins by 1', function(assert) {
+    const kill = ['poor'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 1, mercer: 0, perkins: 1 });
 });
