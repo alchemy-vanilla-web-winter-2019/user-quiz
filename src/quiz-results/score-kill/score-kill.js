af095c3ab92ef45bbe30ac27db8768dbd2670048
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-13 19:36:00 -0800

    more tests

--- src/quiz-results/score-kill/score-kill.js
+++ src/quiz-results/score-kill/score-kill.js
@@ -1,31 +1,56 @@
 function scoreKill(kill, scorecard) {    
-    if(kill.length === 1) {
-        switch(kill[0]) {
-            case 'honor':
-                scorecard.mercer += 1;
-                scorecard.colville += 0.5;
-                scorecard.perkins += 0.5;
-                break;
-            case 'dishonor':
-                scorecard.mercer -= 1;        
-                scorecard.perkins += 1;
-                break;
-            case 'drama':
-                scorecard.colville += 1;
-                scorecard.mercer += 1;
-                break;
-            case 'poor':
-                scorecard.colville += 1;
-                scorecard.perkins += 1;
-                break;
-            default:
-                break;
-        }        
-    }
-    if(kill.length === 0) {
-        scorecard.mercer += 1;
-        scorecard.colville -= 1;
-        scorecard.perkins -= 1;
+    switch(kill.length) {
+        case 0:
+            scorecard.mercer += 1;
+            scorecard.colville -= 1;
+            scorecard.perkins -= 1;
+            break;
+        case 1:
+            switch(kill[0]) {
+                case 'honor':
+                    scorecard.mercer += 1;
+                    scorecard.colville += 0.5;
+                    scorecard.perkins += 0.5;
+                    break;
+                case 'dishonor':
+                    scorecard.mercer -= 1;        
+                    scorecard.perkins += 1;
+                    break;
+                case 'drama':
+                    scorecard.colville += 1;
+                    scorecard.mercer += 1;
+                    break;
+                case 'poor':
+                    scorecard.colville += 1;
+                    scorecard.perkins += 1;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case 2:
+            switch(kill[0]) {
+                case 'honor':
+                    switch(kill[1]) {
+                        case 'drama':
+                            scorecard.colville += 0.5;
+                            scorecard.perkins += 0.5;
+                            scorecard.mercer += 1;
+                            break;
+                        case 'poor':
+                            scorecard.colville += 1;
+                            scorecard.mercer += 0.5;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
     }
 }
 
--- test/quiz-results/score-kill/score-kill.test.js
+++ test/quiz-results/score-kill/score-kill.test.js
@@ -39,3 +39,27 @@ test('if only poor is chosen increment colville and perkins by 1', function(asse
     scoreKill(kill, scorecard);
     assert.deepEqual(scorecard, { colville: 1, mercer: 0, perkins: 1 });
 });
+
+test('honor and dishonor no change', function(assert) {
+    const kill = ['honor', 'dishonor'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 0, mercer: 0, perkins: 0 });
+});
+
+test('honor and drama increment mercer by 1, colville and perkins by 0.5', function(assert) {
+    const kill = ['honor', 'drama'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 0.5, mercer: 1, perkins: 0.5 });
+});
+
+test('honor and poor increment colville by 1, mercer by 0.5', function(assert) {
+    const kill = ['honor', 'poor'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 1, mercer: 0.5, perkins: 0 });
+});
+
+test('dishonor and drama increment perkins by 1, colville by 0.5', function(assert) {
+    const kill = ['dishonor', 'drama'];
+    scoreKill(kill, scorecard);
+    assert.deepEqual(scorecard, { colville: 0.5, mercer: 0, perkins: 1 });
+});
