1fb45150a4323698cc4021c939515a6439fd5ea2
Parker Helland <hellandp@gmail.com>
2019-02-19 17:47:06 -0800

    needs lint fix

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,9 +1,10 @@
-
+import finalResult from '../final-results-page/final-results-page.js';
 
 const quizForm = document.getElementById('quiz-form');
 
-const jsonString = window.localStorage.getItem('username-object');
-const username = JSON.parse(jsonString);
+// const jsonString = window.localStorage.getItem('username-object');
+// const username = JSON.parse(jsonString);  <----same as below---->
+const username = JSON.parse(window.localStorage.getItem('username-object'));
 
 document.getElementById('usersname').innerHTML = username;
 
@@ -15,7 +16,14 @@ quizForm.addEventListener('submit', function(event) {
     const answers = {
         food: formData.get('food'),
     };
-    window.location = 'final-results.html';
+
+    const quizEndResult = finalResult(answers);
+    username.result = quizEndResult;
+
+    const youAreString = JSON.stringify(username);
+    window.localStorage.setItem('endResultObject', youAreString);
+
+    // window.location = 'final-results.html';
     console.log(answers);
 
 });
